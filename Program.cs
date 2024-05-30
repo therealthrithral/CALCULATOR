91e894f3c6c3bf473c975ee7961f36da66575965
therealthrithral <141862468+therealthrithral@users.noreply.github.com>
2024-05-30 09:24:52 -0400

    Added Addition switch case (no support for invalid inputs)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ readResult = readResult.Trim().ToUpper();
 
 if (readResult.Equals("START"))
 {
-    // RunCalculator()
+    RunCalculator();
 }
 else
 {
@@ -15,5 +15,38 @@ else
 
 void RunCalculator()
 {
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
     Console.WriteLine("Please enter the symbol for the corresponding equation (/, +, -, *)");
+    readResult = Console.ReadLine();
+    switch (readResult)
+    {
+        case "+":
+            Console.WriteLine("You chose Addition. ");
+            Console.WriteLine("Please enter your first number");
+            readResult = Console.ReadLine();
+            if (Int32.TryParse(readResult, out firstNumber))
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine("String could not be parsed");
+            }
+            Console.WriteLine("Please enter your second number");
+            readResult = Console.ReadLine();
+            if (Int32.TryParse(readResult, out secondNumber))
+            {
+                Console.WriteLine(secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("String could not be parsed");
+            }
+
+            result = firstNumber + secondNumber;
+            Console.WriteLine($"Your answer is: {firstNumber} + {secondNumber} = {result}");
+            break;
+    }
 }
