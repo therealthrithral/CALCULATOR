0fab8feece27697184c3b1da47e231c703c41cd2
therealthrithral <141862468+therealthrithral@users.noreply.github.com>
2024-05-30 09:03:33 -0400

    Initial Commit for Calculator Console App (C#)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Welcome to my Calculator.  Type in START to begin!");
+
+string readResult = Console.ReadLine();
+
+readResult = readResult.Trim().ToUpper();
+
+if (readResult.Equals("START"))
+{
+    // RunCalculator()
+}
+else
+{
+    Console.WriteLine("Goodbye!");
+}
+
+void RunCalculator()
+{
+    Console.WriteLine("Please enter the symbol for the corresponding equation (/, +, -, *)");
+}
